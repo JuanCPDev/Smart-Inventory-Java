c7bb26436ab07c63f2aa97ac08822c0496c6e710
Juan <jperez301@mail.valenciacollege.edu>
2022-02-05 13:14:21 -0500

    init

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+
+    public static void main(String[] args) {
+        String title="Smart Inventory";
+        System.out.println("\t\t\t"+title);
+        System.out.println("Welcome to your inventory management");
+    }
+
+}
